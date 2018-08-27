74381792bedf597d18fde2c6d32f37b715af3c3f
caikefu <806129001@qq.com>
2018-08-27 11:58:55 +0800

    test

--- testWS2/src/com/ckf/direct/invoke/test1/Test.java
+++ testWS2/src/com/ckf/direct/invoke/test1/Test.java
@@ -2,6 +2,8 @@ package com.ckf.direct.invoke.test1;
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("i;m ok"); 
+		System.out.println("i;m ok"); 
 		System.out.println("i;m ok");
 	}
 }
